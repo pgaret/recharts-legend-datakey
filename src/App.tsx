f2108987ff503defc89a6ef747fd396ba43d6c67
Peregrin Garet <peregringaret@gmail.com>
2023-11-29 12:35:47 -0500

    sample

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useCallback } from "react";
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Legend,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
+import type { Payload } from "recharts/types/component/DefaultLegendContent";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface LineDatum {
+  name: string;
+  example: number;
 }
 
-export default App;
+const labels = Array(12).fill((Math.random() * 10).toLocaleString());
+
+const data: LineDatum[] = Array(12)
+  .fill(1)
+  .map((_v, i) => {
+    return {
+      name: labels[i],
+      example: Math.random() * 100,
+    };
+  });
+
+export default function App() {
+  const renderLegendText = useCallback((value: string, entry: Payload) => {
+    const { color } = entry;
+    // Note that this log includes the datakey
+    console.log(entry);
+    return (
+      <span style={{ color }}>
+        {/* Note that Typescript throws an error here, remove the {entry.dataKey} portion to see the log */}
+        {entry.legendIcon}
+      </span>
+    );
+  }, []);
+
+  return (
+    <AreaChart width={600} height={400} data={data}>
+      <CartesianGrid />
+      <XAxis dataKey="example" />
+      <YAxis />
+      <Legend formatter={renderLegendText} />
+      <Tooltip />
+      <Area
+        type="monotone"
+        dataKey="example"
+        stroke="darkBlue"
+        fill="lightBlue"
+        name="Example"
+      />
+    </AreaChart>
+  );
+}
